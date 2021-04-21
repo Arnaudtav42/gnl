dd5085e479077719c1d6a8151eb6263f13bf852a
Arnaud Tavernier <ataverni@c1r8p1.42nice.fr>
2021-04-21 19:48:18 +0200

    normok

---
+++ get_next_line_utils.c
@@ -0,0 +1,13 @@
+#include "get_next_line.h"
+/*
+int ft_cpydecalagebuff(char **line, const char buffer, int count)
+{
+	if(*line)
+	{
+		*line = ft_strnjoin(line, buffer, count);
+		if (!line)
+			return(-1);
+		ft_strncpy(buffer, buffer + count + 1, BUFFER_SIZE - count);
+		return 1;
+	}	
+}	*/ 	
